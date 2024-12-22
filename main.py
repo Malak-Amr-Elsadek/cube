9b1eef88b8979bf06527d964af8871383ffe3606
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-12-22 15:39:24 +0300

    cube

---
+++ main.py
@@ -0,0 +1,11 @@
+def cube(number):
+    return number*number*number
+
+def bythree(number):
+    if number%3==0:
+         return cube(number)
+    else:
+        return False
+    
+number=int(input("Enter any number:"))
+print(bythree(number))
